3f1e8f69e048df3807cc13e6d7cde11db3024ba7
olena-slominska <petrushe4ka@gmail.com>
2020-05-20 20:27:18 +0300

    Verson 20 May

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function() {
-	$('.slider').slick({});
+	$('.slider').slick({
+		prevArrow: '<button class="slick-arrow slick-prev"><img src="images/leftArrow.jpg" alt="prev arrow" /> </button>',
+		nextArrow: '<button class="slick-arrow slick-next"><img src="images/rightArrow.jpg" alt="next arrow" /> </button>',
+		fade: true
+	});
 })
+
+	

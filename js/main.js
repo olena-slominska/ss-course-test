dd119fcedc795bd64b47eb680164cb2960fcd2df
olena-slominska <petrushe4ka@gmail.com>
2020-05-27 15:00:54 +0300

    contact me form added

--- js/main.js
+++ js/main.js
@@ -6,4 +6,18 @@ $(function() {
 	});
 })
 
-	
+
+// top button behaviour
+$(document).ready(function() {
+	$(window).scroll(function() {
+	  if ($(document).scrollTop() > 100) {
+		$("#topBtn").removeClass("hidden").addClass("shown");
+	  } else {
+		$("#topBtn").removeClass("shown").addClass("hidden");
+	  }
+	});
+  });
+// top button leads to the top
+  $("#topBtn").on("click",function() {
+	$("html, body").animate({scrollTop: 0}, 1000);
+ });

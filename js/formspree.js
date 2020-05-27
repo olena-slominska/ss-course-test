7b9a73f4f2601bd949898b96b034fdac0f052c12
olena-slominska <petrushe4ka@gmail.com>
2020-05-27 17:43:13 +0300

    added jQuery to form submission

--- js/formspree.js
+++ js/formspree.js
@@ -1,45 +1,26 @@
-window.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function(){
+function success() {
+$('#my-button').css('display: none');
+$('#my-form-status').html("Thanks!");
+}
 
-    // get the form elements defined in your form HTML above
-    
-    var form = document.getElementById("my-form");
-    var button = document.getElementById("my-form-button");
-    var status = document.getElementById("my-form-status");
+function error() {
+    $('#my-form-status').html("Oops! There was a problem with sending your message. Try again");
+}
 
-    // Success and Error functions for after the form is submitted
-    
-    function success() {
-      form.reset();
-      button.style = "display: none ";
-      status.innerHTML = "Thanks!";
-    }
-
-    function error() {
-      status.innerHTML = "Oops! There was a problem.";
-    }
-
-    // handle the form submission event
-
-    form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
-  });
-  
-  // helper function for sending an AJAX request
-
-  function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
-    };
-    xhr.send(data);
-  }
+$('#my-form').submit(function(ev) {
+// ev.preventDefault();
+$.ajax({
+    method: "post",
+    url: "https://formspree.io/petrushe4ka@gmail.com",
+    data: {
+        email: reply_to.value,
+        name: name.value,
+        message: message.value
+    },
+    dataType: "json",
+    success: success(),
+    error: error()
+});
+});
+});
